d67fa41625f96a3a2eccb32017be5ba3ca13161a
Jesse Phillips <67770393+jessedeanrivero@users.noreply.github.com>
2026-04-10 00:04:04 -0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "jesse-hq-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/breakdown.js",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => (key !== CACHE_NAME ? caches.delete(key) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
